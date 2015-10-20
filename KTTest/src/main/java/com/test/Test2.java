36943d807ae7e553ad8c51e32ec087f108f9de8b
parthadmishra <partha-sarathi.mishra@capgemini.com>
2015-10-19 22:25:15 -0700

    Blasjasldsa

---
+++ KTTest/src/main/java/com/test/Test2.java
@@ -0,0 +1,37 @@
+package com.test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Test2
+ */
+public class Test2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Test2() {
+        // TODO Auto-generated constructor stub
+    	System.err.println("ererr");
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
